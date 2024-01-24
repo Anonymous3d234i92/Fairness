6c202e4f0cbf26674b513d827d3f9effc84987ef
Zeyu Sun <146193399+Anonymous3d234i92@users.noreply.github.com>
2024-01-24 15:27:01 +0800

    upload

--- TestNew.py
+++
@@ -1,6 +0,0 @@
-import os
-
-#os.system("python3 ./MutantGen-Test.py")
-os.system("cp NewThres/TestGenerator-NMTMLT/*.txt NewThres/TestGenerator-NMTMLT/*.index ./NMT_zh_en0-8Mu/pad")
-os.system("cd ./NMT_zh_en0-8Mu/pad && sh desp.sh")
-os.system("cd ./NMT_zh_en0-8Mu/pad && sh test.sh")
